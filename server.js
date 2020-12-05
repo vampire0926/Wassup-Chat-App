cd5765ed497213e1bcc49507043043fec97d4e11
VARUN DUBEY <varun07dubey@gmail.com>
2020-12-05 17:24:15 +0530

    added favicon

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
+const favicon = require('serve-favicon')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5500
 
 http.listen(PORT, () => {
-     console.log(`Listening on port ${PORT}`)
+     console.log(`Yahoo!!! The server is connected on port ${PORT}`)
 })
 
 app.use(express.static(__dirname + '/public'))
-
+app.use(favicon(__dirname + '/public/favicon.ico'))
 
 app.get('/', (req, res) => {
      res.sendFile(__dirname + '/index.html')
@@ -19,7 +20,7 @@ app.get('/', (req, res) => {
 const io = require('socket.io')(http)
 
 io.on('connection', (socket) => {
-     console.log('Connected...')
+     console.log('A User Connected...')
      socket.on('message', (msg) => {
           socket.broadcast.emit('message', msg)
      })
